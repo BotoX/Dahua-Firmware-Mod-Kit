424d2ba1f61fdfd5872a76fdbc5dc5a4427a9515
thanhdiepts <ldtdiep@gmail.com>
2016-10-16 22:48:28 +0700

    Update nvr4xxx-4k.py
    Update correct mtd size

--- configs/nvr4xxx-4k.py
+++ configs/nvr4xxx-4k.py
@@ -1,34 +1,38 @@
 from .config import *
 
 DAHUA_FILES = OrderedDict([
+	("Install.lua", {
+		"required": True,
+		"type": DAHUA_TYPE.Plain
+	}),
 	("u-boot.bin.img", {
-		"required": False,
-		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.cramfs,
-		"size": 0x00010000
+		"required": True,
+		"type": DAHUA_TYPE.Plain,
+		"size": 0x0080000
 	}),
 	("romfs-x.cramfs.img", {
 		"required": True,
-		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.cramfs,
-		"size": 0x0010000
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.CramFS,
+		"size": 0x1700000
 	}),
 	("web-x.cramfs.img", {
 		"required": True,
-		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.cramfs,
-		"size": 0x0010000
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.CramFS,
+		"size": 0x0380000
 	}),
 	("custom-x.cramfs.img", {
 		"required": True,
-		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.cramfs,
-		"size": 0x0010000
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.CramFS,
+		"size": 0x0080000
 	}),
 	("logo-x.cramfs.img", {
 		"required": True,
-		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.cramfs,
-		"size": 0x0010000
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.CramFS,
+		"size": 0x00c0000
 	}),
 	("web_mac-x.cramfs.img", {
 		"required": True,
-		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.cramfs,
-		"size": 0x0010000
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.CramFS,
+		"size": 0x0140000
 	}),
 ])
